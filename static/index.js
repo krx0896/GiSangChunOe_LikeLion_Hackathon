70d0f900c26cb3a33470fda5fa4ad9f79222b076
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-07-27 17:06:32 +0900

    second

--- static/index.js
+++ static/index.js
@@ -1,8 +1,6 @@
 let textEls = document.querySelectorAll('.subtext p')
-console.log(textEls)
-textEls.forEach(textElement => {
-  textElement.style.display="none";
-});
+textEls[1].style.display="none";
+textEls[2].style.display="none";
 function textShow(textEl){
   textEls.forEach(textElement => {
     textElement.style.display="none";
